56d0e0521c2aacc083a828ffe616348388bee685
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-11-21 12:06:41 +0600

    project added

--- src/app/(commonLayout)/projects/page.tsx
+++ src/app/(commonLayout)/projects/page.tsx
@@ -13,6 +13,7 @@ import typescript from "@/src/assets/images/stack/Typescript.svg";
 import vercel from "@/src/assets/images/stack/Vercel.svg";
 import git from "@/src/assets/images/stack/Git.svg";
 import project1 from "@/src/assets/images/project/regal-dragon.png";
+import Container from "@/src/components/Container";
 
 export default function Projects() {
   const projects = [
@@ -70,61 +71,73 @@ export default function Projects() {
     },
   ];
   return (
-    <div className="mt-8 lg:mt-20">
-      <h2 className="text-3xl lg:text-5xl font-bold text-center text-red-400">
-        Projects
-      </h2>
-      <div className="w-full flex flex-col gap-20 justify-center items-center mt-8 lg:mt-20">
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            className="relative bg-cover bg-no-repeat w-4/5 h-[500px] shadow-[0px_0px_40px_#1f1f1f] group"
-            style={{
-              backgroundImage: `url(${project.imageUrl})`,
-            }}
-          >
-            <div className="absolute inset-0 bg-[#1f1f1f9a]"></div>
-            <div className="absolute inset-0 bg-gradient-to-r from-[#343d68] to-[#343d687c] scale-x-0 origin-left transition-transform duration-400 group-hover:scale-x-100"></div>
-            <div className="absolute text-white text-[100px] font-bold hidden group-hover:block right-[-10px] top-[-40px]">
-              {project.id}
-            </div>
-            <div className="absolute flex flex-col gap-5 text-white bottom-[23%] left-[8%] z-20 transform group-hover:scale-110 transition-transform duration-400 lg:w-1/2">
-              <h1 className="text-lg lg:text-4xl font-bold text-center lg:text-start lg:mb-3">
-                {project.title}
-              </h1>
-              <p className="text-sm lg:text-lg">{project.description}</p>
-              <div className="flex gap-3">
-                {project.techStack.map((tech, index) => (
-                  <span
-                    key={index}
-                    className="text-xs bg-white text-gray-800 rounded-full px-3 py-1"
+    <>
+      <div className="mt-8 lg:mt-20 px-4">
+        <h2 className="text-3xl lg:text-5xl font-bold text-center text-accent">
+          Projects
+        </h2>
+        <div className="w-full flex flex-col gap-12 justify-center items-center mt-8 lg:mt-20">
+          {projects.map((project) => (
+            <div
+              key={project.id}
+              className="relative bg-cover bg-no-repeat w-full sm:w-4/5 lg:w-3/4 min-h-[350px] sm:min-h-[500px] lg:min-h-[600px] shadow-[0px_0px_40px_#1f1f1f] group"
+              style={{
+                backgroundImage: `url(${project.imageUrl})`,
+                backgroundPosition: "center",
+                backgroundSize: "cover", // Ensures the image covers the whole div
+              }}
+            >
+              <div className="absolute inset-0 bg-[#1f1f1f9a]"></div>
+              <div className="absolute inset-0 bg-gradient-to-r from-[#343d68] to-[#343d687c] scale-x-0 origin-left transition-transform duration-400 group-hover:scale-x-100"></div>
+              <div className="absolute text-white text-[80px] sm:text-[100px] font-bold hidden group-hover:block right-[-10px] top-[-40px]">
+                {project.id}
+              </div>
+              <div className="absolute flex flex-col gap-5 text-white bottom-[23%] left-[8%] z-20 transform group-hover:scale-110 transition-transform duration-400 lg:w-1/2 sm:w-3/4 md:w-2/3">
+                <h1 className="text-lg lg:text-4xl font-bold text-center lg:text-start lg:mb-3">
+                  {project.title}
+                </h1>
+                <p className="text-sm lg:text-lg">{project.description}</p>
+                <div className="flex gap-3 flex-wrap">
+                  {project.techStack.map((tech, index) => (
+                    <span
+                      key={index}
+                      className="text-xs bg-white text-gray-800 rounded-full px-3 py-1"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+                <div className="mt-4 flex gap-4 flex-wrap">
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-teal-500 text-white px-4 py-2 rounded hover:bg-teal-600"
                   >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="mt-4 flex gap-4">
-                <a
-                  href={project.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                >
-                  GitHub
-                </a>
-                <a
-                  href={project.live}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-                >
-                  Live Demo
-                </a>
+                    Read More
+                  </a>
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                  >
+                    GitHub
+                  </a>
+                  <a
+                    href={project.live}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                  >
+                    Live Demo
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/components/UI/footer/Footer.tsx
+++ src/components/UI/footer/Footer.tsx
@@ -1,91 +1,94 @@
 import NextLink from "next/link";
 import { FaFacebook, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import Container from "../../Container";
 
 export default function Footer() {
   return (
-    <div className="mt-20 w-full flex items-center justify-center text-default-800 px-4">
-      <div className="md:w-2/3 w-full px-4 flex flex-col justify-center">
-        <div className="w-full text-3xl lg:text-6xl font-bold mt-6">
-          <h1 className="w-full md:w-2/3">Get in Touch</h1>
-        </div>
-        <div className="flex mt-8 flex-col md:flex-row md:justify-between items-start ">
-          <p className="w-full md:w-2/3   text-justify">
-            I'm always open to new opportunities, collaboration, and connecting
-            with fellow developers. Feel free to reach out to me via Email or
-            connect with me on LinkedIn
-          </p>
-          <div className="lg:w-[20%] w-full flex justify-center mt-3 lg:mt-0 lg:pt-0 ">
-            <NextLink
-              href="/contact"
-              className="bg-accent justify-center text-center rounded-lg shadow px-10 py-3 flex items-center text-white"
-            >
-              Contact US
-            </NextLink>
+    <Container>
+      <div className="mt-20 w-full flex items-center justify-center text-default-800 px-4">
+        <div className=" w-full px-4 flex flex-col justify-center">
+          <div className="w-full text-3xl lg:text-6xl font-bold mt-6">
+            <h1 className="w-full md:w-2/3">Get in Touch</h1>
           </div>
-        </div>
-        <div className="flex flex-col">
-          <div className=" flex mt-4 lg:mt-24  flex-row justify-around lg:justify-between">
-            <NextLink
-              href="/"
-              className="hidden md:block cursor-pointer font-bold  font-stylish-one uppercase text-xl"
-            >
-              Al Samiul
-            </NextLink>
-            <NextLink
-              href="/about"
-              className=" md:block cursor-pointer  uppercase"
-            >
-              About
-            </NextLink>
-            <NextLink
-              href="/skills"
-              className=" md:block cursor-pointer   uppercase"
-            >
-              Skill
-            </NextLink>
-            <NextLink
-              href="/projects"
-              className=" md:block cursor-pointer uppercase"
-            >
-              Project
-            </NextLink>
-            <NextLink
-              href="/contact"
-              className=" md:block cursor-pointer uppercase"
-            >
-              Contact
-            </NextLink>
+          <div className="flex mt-8 flex-col md:flex-row md:justify-between items-start ">
+            <p className="w-full md:w-2/3   text-justify">
+              I'm always open to new opportunities, collaboration, and
+              connecting with fellow developers. Feel free to reach out to me
+              via Email or connect with me on LinkedIn
+            </p>
+            <div className="lg:w-[20%] w-full flex justify-center mt-3 lg:mt-0 lg:pt-0 ">
+              <NextLink
+                href="/contact"
+                className="bg-accent justify-center text-center rounded-lg shadow px-10 py-3 flex items-center text-white"
+              >
+                Contact US
+              </NextLink>
+            </div>
           </div>
-          <div className=" flex gap-4 justify-center lg:items-start mt-4 lg:mt-6">
-            <a
-              target="_blank"
-              href="https://www.facebook.com/alsamiul1996"
-              rel="noreferrer"
-            >
-              <FaFacebook className="text-3xl hover:text-accent " />
-            </a>
+          <div className="flex flex-col">
+            <div className=" flex mt-4 lg:mt-24  flex-row justify-around lg:justify-between">
+              <NextLink
+                href="/"
+                className="hidden md:block cursor-pointer font-bold  font-stylish-one uppercase text-xl"
+              >
+                Al Samiul
+              </NextLink>
+              <NextLink
+                href="/about"
+                className=" md:block cursor-pointer  uppercase"
+              >
+                About
+              </NextLink>
+              <NextLink
+                href="/skills"
+                className=" md:block cursor-pointer   uppercase"
+              >
+                Skill
+              </NextLink>
+              <NextLink
+                href="/projects"
+                className=" md:block cursor-pointer uppercase"
+              >
+                Project
+              </NextLink>
+              <NextLink
+                href="/contact"
+                className=" md:block cursor-pointer uppercase"
+              >
+                Contact
+              </NextLink>
+            </div>
+            <div className=" flex gap-4 justify-center lg:items-start mt-4 lg:mt-6">
+              <a
+                target="_blank"
+                href="https://www.facebook.com/alsamiul1996"
+                rel="noreferrer"
+              >
+                <FaFacebook className="text-3xl hover:text-accent " />
+              </a>
 
-            <a
-              target="_blank"
-              href="https://github.com/mohammad-al-samiul"
-              rel="noreferrer"
-            >
-              <FaGithub className="text-3xl hover:text-accent transition-all duration-600" />
-            </a>
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/al-samiul/"
-              rel="noreferrer"
-            >
-              <FaLinkedinIn className="text-3xl hover:text-accent transition-all duration-600" />
-            </a>
+              <a
+                target="_blank"
+                href="https://github.com/mohammad-al-samiul"
+                rel="noreferrer"
+              >
+                <FaGithub className="text-3xl hover:text-accent transition-all duration-600" />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/al-samiul/"
+                rel="noreferrer"
+              >
+                <FaLinkedinIn className="text-3xl hover:text-accent transition-all duration-600" />
+              </a>
+            </div>
+            <hr className="border-gray-600 mt-7" />
+            <p className="w-full text-center my-12 ">
+              Copyright © 2023 Al Samiul Creative
+            </p>
           </div>
-          <hr className="border-gray-600 mt-7" />
-          <p className="w-full text-center my-12 ">
-            Copyright © 2023 Al Samiul Creative
-          </p>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
